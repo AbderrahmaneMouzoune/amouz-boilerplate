d150b98ba3fcf9c4b0dcf55af34b09bb66a4e317
AbderrahmaneMouzoune <abderrahmane.mouzoune@gmail.com>
2024-05-22 01:58:58 +0200

    feat: add prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Simple boilerplate by Abderrahmane Mouzoune",
-  description: "Simple boilerplate by Abderrahmane Mouzoune",
+  title: 'Simple boilerplate by Abderrahmane Mouzoune',
+  description: 'Simple boilerplate by Abderrahmane Mouzoune',
 }
 
 export default function RootLayout({

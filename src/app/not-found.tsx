2082d3aa114a0ec8a9d9ae27d7b437185bc60a22
AbderrahmaneMouzoune <abderrahmanemouzoune@gmail.com>
2024-09-25 17:27:58 +0200

    refactor: :building_construction: center not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,9 @@ import { Headline } from '@components/ui/headline'
 export default function NotFound() {
   return (
     <section className="container mx-auto space-y-8">
-      <Headline variant={'h1'}>404 Page not found :(</Headline>
+      <Headline variant={'h1'} className="text-center">
+        404 Page not found :(
+      </Headline>
     </section>
   )
 }

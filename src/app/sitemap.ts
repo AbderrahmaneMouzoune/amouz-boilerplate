0b65a449c17a35eeb1f8ccfce3bb5a2fb3004e4f
AbderrahmaneMouzoune <abderrahmanemouzoune@gmail.com>
2024-09-25 11:26:37 +0200

    feat((improve-seo)): :sparkles: improve seo with robot.txt & sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,13 @@
+import { env } from '@/env.mjs'
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+  ]
+}
